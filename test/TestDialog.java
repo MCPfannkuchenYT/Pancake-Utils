764d66a4ff57c6e49633083d2f2d4036a73f6836
MCPfannkuchenYT <gamesupport@mgnet.work>
2021-10-10 00:29:57 +0200

    Tests for Dialog Utils

---
+++ test/TestDialog.java
@@ -0,0 +1,34 @@
+import de.pancakeutils.Dialog;
+
+public class TestDialog {
+
+	public static void main(String[] args) {
+		/* TEST Dialog.enableSystemStyle */
+		System.out.print("Dialog Objects.enableSystemStyle");
+		try {
+			Dialog.enableSystemStyle();
+			System.out.println(" - Check passed, successfully changed to system style");
+		} catch (Exception e) {
+			if ("Unable to change ui look and feel".equals(e.getMessage())) {
+				System.out.println(" - Check passed, could not switch to system style");
+			} else {
+				System.out.println(" - Check failed, unknown error message");
+				e.printStackTrace();
+				return;
+			}
+		}
+		/* TEST Dialog.askForUserInput */
+		System.out.print("Dialog Objects.askForUserInput");
+		try {
+			Dialog.askForUserInput(null, null);
+			System.out.println(" - Check failed");
+		} catch (RuntimeException e) {
+			System.out.println(" - Check passed");
+		} catch (Exception e) {
+			System.out.println(" - Check failed, unknown error");
+			e.printStackTrace();
+		}
+		System.out.println("-------------------------------");
+	}
+	
+}
